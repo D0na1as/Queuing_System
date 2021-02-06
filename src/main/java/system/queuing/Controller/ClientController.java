ea8d80799bfa82f064898e4564b9936af8cf5e04
D0na1as <d.kadakauskis@gmail.com>
2021-02-06 22:26:20 +0200

    Determined TODO in Controllers

--- src/main/java/system/queuing/Controller/ClientController.java
+++ src/main/java/system/queuing/Controller/ClientController.java
@@ -6,4 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/client")
 public class ClientController {
+
+    //TODO
+    // 1. Appointment reservation
+    // 2. Checking appointment
+
 }
--- src/main/java/system/queuing/Controller/UserController.java
+++ src/main/java/system/queuing/Controller/UserController.java
@@ -6,4 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    //TODO
+    // 1. Login
+    // 2. Clients list
+    // 2.1. Mark visit begun
+    // 2.2. Mark visit ended
+    // 2.3. Mark visit canceled
+
 }
