cfdffd48f2dd3b620f634fb4a34dcf85bfafc7ea
donatas <d.kadakauskis@gmail.com>
2021-04-22 19:18:13 +0300

    Access denied page

--- src/main/java/system/queuing/Repository/ClientRepo.java
+++ src/main/java/system/queuing/Repository/ClientRepo.java
@@ -24,7 +24,7 @@ public interface ClientRepo extends CrudRepository<Client, Integer> {
     String getClientS = "SELECT * FROM "+ table +" WHERE user=?1 AND date=?2";
     String getCount = "SELECT COUNT(id) FROM "+ table +" WHERE user=?1 AND NOT status='canceled' AND date=?2";
     String getLastInQue = "SELECT IFNULL((SELECT que_nr FROM "+ table +" WHERE user=?1 AND NOT status='canceled' AND date=?2 ORDER BY que_nr DESC LIMIT 1), 0)";
-    String getQueNr = "SELECT que_nr FROM "+ table +" WHERE user=?1 AND date=?2 AND status=:#{#status.name()}";
+    String getQueNr = "SELECT IFNULL((SELECT que_nr FROM "+ table +" WHERE user=?1 AND date=?2 AND status=:#{#status.name()}), 0)";
     String getQue = "SELECT IFNULL(que_nr, 0) from (SELECT que_nr FROM "+ table +" WHERE user=?1 AND status='registered' AND date=?2) as tbl  ORDER BY que_nr ASC LIMIT ?3";
     String getQueLeft = "SELECT COUNT(id) FROM "+ table +" WHERE user=?1 AND status=:#{#status.name()} AND que_nr<?2";
 
